44ebb4626e88d0281d0fb5d3d8f5eef61cbd8d72
ponzu-4C9 <baijingseiya@gmail.com>
2026-02-07 14:38:25 +0900

    ログ出力を改善し、POST、PUT、DELETEメソッドでの受信データを明示化

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -12,12 +12,13 @@ const pool = new Pool({
 
 export async function GET() {
     const result = await pool.query('SELECT * FROM posts ORDER BY created_at ASC');
+    
     return NextResponse.json(result.rows);
 }
 
 export async function POST(request: Request) {
     const txt  = (await request.json()).mainText;
-    console.log("received mainText:", txt);
+    console.log("POST:", txt);
     const result = await pool.query(
         'INSERT INTO posts (main_text) VALUES ($1) RETURNING *',
         [txt]
@@ -27,6 +28,7 @@ export async function POST(request: Request) {
 
 export async function PUT(request: Request) {
     const { id, mainText } = await request.json();
+    console.log("PUT:", id, mainText);
     const result = await pool.query(
         'UPDATE posts SET main_text = $1 WHERE id = $2 RETURNING *',
         [mainText, id]
@@ -36,6 +38,7 @@ export async function PUT(request: Request) {
 
 export async function DELETE(request: Request) {
     const id = await request.json();
+    console.log("DELETE:", id);
     const result = await pool.query('DELETE FROM posts WHERE id = $1', [id]);
     return NextResponse.json({ message: 'Deleted' });
 }
